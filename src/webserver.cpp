7aa6f3790fdab551ef56668365d82f14eda38033
joesei <josemhernandez3713@gmail.com>
2022-03-23 22:15:21 -0700

    Add project files.

---
+++ src/webserver.cpp
@@ -0,0 +1,12 @@
+﻿// webserver.cpp : Defines the entry point for the application.
+//
+
+#include "webserver.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
